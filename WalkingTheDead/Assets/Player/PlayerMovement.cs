9d1d7620246299e9407f89a6536e746327357127
Varun Naval <varun.naval@gmail.com>
2019-04-03 14:37:36 +1300

    Added moving player as well as scrolling camera

---
+++ WalkingTheDead/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float walkSpeed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(walkSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0.0f, walkSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+    }
+}
